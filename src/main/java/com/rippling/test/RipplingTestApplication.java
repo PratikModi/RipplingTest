48eee3a3022559f0e28b5b9fb3b7e371340cb079
Pratik Modi <Pratik.Modi@gmail.com>
2023-09-26 22:49:47 +0530

    Rippling Coding Test

---
+++ src/main/java/com/rippling/test/RipplingTestApplication.java
@@ -0,0 +1,13 @@
+package com.rippling.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipplingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RipplingTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rippling/test/RipplingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rippling.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RipplingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
